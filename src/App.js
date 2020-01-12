a4e27bce0817238f6e1d863a3a94673ac33acb6f
Domo <stolarczyk.dominik@gmail.com>
2020-01-12 18:54:04 +0100

    adding to the list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import ListItem from './components/ListItem';
-import AddItem from './components/AddItem';
 
 function App() {
+	const [names, setNames] = useState(['John Doe']);
+
+	const addName = newName => {
+		console.log('newName', newName);
+		setNames([...names, newName]);
+		console.log('names', names);
+	};
+
 	return (
 		<div className="App">
 			<header className="App-header">Filtering</header>
 			<main className="App-content">
-				{['frank', 'jane'].map(person => (
-					<ListItem name={person}></ListItem>
+				{names.map(name => (
+					<ListItem name={name}></ListItem>
 				))}
-				<AddItem></AddItem>
+				<button onClick={() => addName('Jane Doe')}>add</button>
 			</main>
 			<footer className="App-footer">Footer</footer>
 		</div>
