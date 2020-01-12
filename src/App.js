4899a2cd1e51978ea1e1afe48ee737ef056b6ce8
Domo <stolarczyk.dominik@gmail.com>
2020-01-12 15:02:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">Filtering</header>
+			<main className="App-content">
+				{['frank', 'jane'].map(person => (
+					<ListItem name={person}></ListItem>
+				))}
+				<AddItem></AddItem>
+			</main>
+			<footer className="App-footer">Footer</footer>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/AddItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AddItem() {
+	return (
+		<div>
+			<input type="text" />
+			<button>Add</button>
+		</div>
+	);
+}
